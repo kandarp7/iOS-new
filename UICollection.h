ff577afc2eef2b27d26029a9d6501bbefc8fe4be
Kandarp Dave <kandarp7@hotmail.com>
2014-05-12 18:30:54 -0400

    1st commit

---
+++ UICollection.h
@@ -0,0 +1,13 @@
+//
+//  UICollection.h
+//  GitHubProj
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 GMolton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UICollection : NSObject
+
+@end
---
+++ UICollection.m
@@ -0,0 +1,13 @@
+//
+//  UICollection.m
+//  GitHubProj
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 GMolton. All rights reserved.
+//
+
+#import "UICollection.h"
+
+@implementation UICollection
+
+@end
