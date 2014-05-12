93a9705f4a01fe535d10c3542addd5bf77bc81c8
Kandarp Dave <kandarp7@hotmail.com>
2014-05-12 18:23:50 -0400

    Initial Commit

---
+++ GitHubProj/UICollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  UICollectionViewController.h
+//  GitHubProj
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 GMolton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionViewController : UIViewController
+
+@end
---
+++ GitHubProj/UICollectionViewController.m
@@ -0,0 +1,29 @@
+//
+//  UICollectionViewController.m
+//  GitHubProj
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 GMolton. All rights reserved.
+//
+
+#import "UICollectionViewController.h"
+
+@interface UICollectionViewController ()
+
+@end
+
+@implementation UICollectionViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
